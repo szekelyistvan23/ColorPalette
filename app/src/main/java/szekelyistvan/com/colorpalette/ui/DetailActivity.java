4704169b1cababa9eb7c3e83713797b511a57f76
Szekely Istvan <steven.sekler@gmail.com>
2018-06-21 16:58:46 +0300

    feat: DetailActivity added

---
+++ app/src/main/java/szekelyistvan/com/colorpalette/ui/DetailActivity.java
@@ -0,0 +1,15 @@
+package szekelyistvan.com.colorpalette.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import szekelyistvan.com.colorpalette.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
