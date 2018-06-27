d6a4149e67c0b2baaf94bc024b8fcabb7b4d01a3
Szekely Istvan <steven.sekler@gmail.com>
2018-06-27 13:06:03 +0300

    fix: DbHelper class modified

--- app/src/main/java/szekelyistvan/com/colorpalette/provider/PaletteContract.java
+++ app/src/main/java/szekelyistvan/com/colorpalette/provider/PaletteContract.java
@@ -9,31 +9,49 @@ public class PaletteContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
     public static final String PATH_PALETTES = "palettes";
 
-    public static final class FavouritesEntry implements BaseColumns {
+    public static final class PaletteEntry implements BaseColumns {
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_PALETTES).build();
 
         public static final String DB_NAME = "palettes.db";
         public static final int DB_VERSION = 1;
-        public static final String TABLE_NAME = "palettes";
-        public static final String FAVOURITES_COLUMN_ID = "PALETTE_ID";
-        public static final String FAVOURITES_COLUMN_PALETTE_NAME = "PALETTE_NAME";
-        public static final String FAVOURITES_COLUMN_COLOR_ONE = "COLOR_ONE";
-        public static final String FAVOURITES_COLUMN_COLOR_TWO = "COLOR_TWO";
-        public static final String FAVOURITES_COLUMN_COLOR_THREE = "COLOR_THREE";
-        public static final String FAVOURITES_COLUMN_COLOR_FOUR = "COLOR_FOUR";
-        public static final String FAVOURITES_COLUMN_COLOR_FIVE = "COLOR_FIVE";
-        public static final String FAVOURITES_COLUMN_FAVORITE = "FAVORITE";
-        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " ("
+        public static final String TABLE_NAME_TOP = "top";
+        public static final String TABLE_NAME_NEW = "new";
+        public static final String TABLE_NAME_FAVORITE = "favorite";
+        public static final String PALETTES_COLUMN_ID = "PALETTE_ID";
+        public static final String PALETTES_COLUMN_PALETTE_NAME = "PALETTE_NAME";
+        public static final String PALETTES_COLUMN_COLOR_ONE = "COLOR_ONE";
+        public static final String PALETTES_COLUMN_COLOR_TWO = "COLOR_TWO";
+        public static final String PALETTES_COLUMN_COLOR_THREE = "COLOR_THREE";
+        public static final String PALETTES_COLUMN_COLOR_FOUR = "COLOR_FOUR";
+        public static final String PALETTES_COLUMN_COLOR_FIVE = "COLOR_FIVE";
+        public static final String CREATE_TABLE_TOP = "CREATE TABLE " + TABLE_NAME_TOP + " ("
                 + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + FAVOURITES_COLUMN_ID + " TEXT, "
-                + FAVOURITES_COLUMN_PALETTE_NAME + " TEXT, "
-                + FAVOURITES_COLUMN_COLOR_ONE + " TEXT, "
-                + FAVOURITES_COLUMN_COLOR_TWO + " TEXT, "
-                + FAVOURITES_COLUMN_COLOR_THREE + " TEXT, "
-                + FAVOURITES_COLUMN_COLOR_FOUR + " TEXT, "
-                + FAVOURITES_COLUMN_COLOR_FIVE + " TEXT, "
-                + FAVOURITES_COLUMN_FAVORITE + " TEXT);";
+                + PALETTES_COLUMN_ID + " TEXT, "
+                + PALETTES_COLUMN_PALETTE_NAME + " TEXT, "
+                + PALETTES_COLUMN_COLOR_ONE + " TEXT, "
+                + PALETTES_COLUMN_COLOR_TWO + " TEXT, "
+                + PALETTES_COLUMN_COLOR_THREE + " TEXT, "
+                + PALETTES_COLUMN_COLOR_FOUR + " TEXT, "
+                + PALETTES_COLUMN_COLOR_FIVE + " TEXT);";
+        public static final String CREATE_TABLE_NEW = "CREATE TABLE " + TABLE_NAME_NEW + " ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PALETTES_COLUMN_ID + " TEXT, "
+                + PALETTES_COLUMN_PALETTE_NAME + " TEXT, "
+                + PALETTES_COLUMN_COLOR_ONE + " TEXT, "
+                + PALETTES_COLUMN_COLOR_TWO + " TEXT, "
+                + PALETTES_COLUMN_COLOR_THREE + " TEXT, "
+                + PALETTES_COLUMN_COLOR_FOUR + " TEXT, "
+                + PALETTES_COLUMN_COLOR_FIVE + " TEXT);";
+        public static final String CREATE_TABLE_FAVORITE = "CREATE TABLE " + TABLE_NAME_FAVORITE + " ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PALETTES_COLUMN_ID + " TEXT, "
+                + PALETTES_COLUMN_PALETTE_NAME + " TEXT, "
+                + PALETTES_COLUMN_COLOR_ONE + " TEXT, "
+                + PALETTES_COLUMN_COLOR_TWO + " TEXT, "
+                + PALETTES_COLUMN_COLOR_THREE + " TEXT, "
+                + PALETTES_COLUMN_COLOR_FOUR + " TEXT, "
+                + PALETTES_COLUMN_COLOR_FIVE + " TEXT);";
     }
 }
