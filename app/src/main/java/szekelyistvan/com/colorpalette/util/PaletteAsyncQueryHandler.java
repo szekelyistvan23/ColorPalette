6aa5903bae8c908e9a47adae9c7ca859f4d1ef4f
Szekely Istvan <steven.sekler@gmail.com>
2018-07-03 12:33:52 +0300

    feat: OnQueryComplete method override

--- app/src/main/java/szekelyistvan/com/colorpalette/util/PaletteAsyncQueryHandler.java
+++ app/src/main/java/szekelyistvan/com/colorpalette/util/PaletteAsyncQueryHandler.java
@@ -2,10 +2,27 @@ package szekelyistvan.com.colorpalette.util;
 
 import android.content.AsyncQueryHandler;
 import android.content.ContentResolver;
+import android.database.Cursor;
 
 public class PaletteAsyncQueryHandler extends AsyncQueryHandler{
 
+    private AsyncQueryListener listener;
+
     public PaletteAsyncQueryHandler(ContentResolver cr) {
         super(cr);
+    }
+
+    public PaletteAsyncQueryHandler(ContentResolver cr, AsyncQueryListener listener) {
+        super(cr);
+        this.listener = listener;
+    }
+
+    public interface AsyncQueryListener{
+        void onQueryComplete(Cursor cursor);
+    }
+
+    @Override
+    protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+            listener.onQueryComplete(cursor);
     }
 }
