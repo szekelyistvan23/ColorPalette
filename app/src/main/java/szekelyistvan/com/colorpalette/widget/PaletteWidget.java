012d5d994663ce67c0a2039f1a4eb231dc9522e9
Szekely Istvan <steven.sekler@gmail.com>
2018-07-08 20:48:16 +0300

    fix: Unnecessary method removed

--- app/src/main/java/szekelyistvan/com/colorpalette/widget/PaletteWidget.java
+++ app/src/main/java/szekelyistvan/com/colorpalette/widget/PaletteWidget.java
@@ -19,18 +19,6 @@ public class PaletteWidget extends AppWidgetProvider {
 
     public static final String POSITION_FROM_WIDGET ="position_from_widget";
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.palette_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
