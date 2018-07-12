01ce43ac61571cddea713ff488fb9b914c804888
Szekely Istvan <steven.sekler@gmail.com>
2018-07-12 13:45:17 +0300

    feat: Snackbar's behavior modified

---
+++ app/src/main/java/szekelyistvan/com/colorpalette/util/PaletteBottomNavigationBehavior.java
@@ -0,0 +1,57 @@
+package szekelyistvan.com.colorpalette.util;
+
+/**
+ * Based on: https://android.jlelse.eu/scroll-your-bottom-navigation-view-away-with-10-lines-of-code-346f1ed40e9e
+ */
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+class PaletteBottomNavigationBehavior <V extends View> extends CoordinatorLayout.Behavior<V> {
+
+    public PaletteBottomNavigationBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull V child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onNestedPreScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull V child, @NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        child.setTranslationY(max(0f, min((float)child.getHeight(), child.getTranslationY() + dy)));
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, V child, View dependency) {
+        if (dependency instanceof Snackbar.SnackbarLayout){
+            updateSnackbar(child, (Snackbar.SnackbarLayout)dependency);
+        }
+        return super.layoutDependsOn(parent, child, dependency);
+    }
+
+
+    private void updateSnackbar(View child, Snackbar.SnackbarLayout snackbarLayout) {
+        if (snackbarLayout.getLayoutParams() instanceof CoordinatorLayout.LayoutParams) {
+            CoordinatorLayout.LayoutParams params = null;
+
+            params.setAnchorId(child.getId());
+            params.anchorGravity = Gravity.TOP;
+            params.gravity = Gravity.TOP;
+            snackbarLayout.setLayoutParams(params);
+        }
+    }
+}
