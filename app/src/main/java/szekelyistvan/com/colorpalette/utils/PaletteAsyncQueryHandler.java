e8954b2a61b3ab84c6bf604a17735acd30ea323d
Szekely Istvan <steven.sekler@gmail.com>
2018-07-13 12:02:51 +0300

    fix: Listener removed from the class

--- app/src/main/java/szekelyistvan/com/colorpalette/utils/PaletteAsyncQueryHandler.java
+++ app/src/main/java/szekelyistvan/com/colorpalette/utils/PaletteAsyncQueryHandler.java
@@ -5,24 +5,7 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 
 public class PaletteAsyncQueryHandler extends AsyncQueryHandler{
-
-    private AsyncQueryListener listener;
-
     public PaletteAsyncQueryHandler(ContentResolver cr) {
         super(cr);
-    }
-
-    public PaletteAsyncQueryHandler(ContentResolver cr, AsyncQueryListener listener) {
-        super(cr);
-        this.listener = listener;
-    }
-
-    public interface AsyncQueryListener{
-        void onQueryComplete(Cursor cursor);
-    }
-
-    @Override
-    protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
-            listener.onQueryComplete(cursor);
     }
 }
