f117bcb27bebc3fdcce2abf3ea8fddd75f9ec93c
Szekely Istvan <steven.sekler@gmail.com>
2018-07-13 11:22:17 +0300

    feat: Method to set loader args

---
+++ app/src/main/java/szekelyistvan/com/colorpalette/utils/LoaderUtil.java
@@ -0,0 +1,26 @@
+package szekelyistvan.com.colorpalette.utils;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import static szekelyistvan.com.colorpalette.provider.PaletteLoader.LOADER_SELECTION;
+import static szekelyistvan.com.colorpalette.provider.PaletteLoader.LOADER_SELECTION_ARGS;
+import static szekelyistvan.com.colorpalette.provider.PaletteLoader.LOADER_URI;
+
+public class LoaderUtil {
+    public static Bundle makeBundle(Uri uri, String selection, String selectionArgs){
+        Bundle args = new Bundle();
+        args.putString(LOADER_URI, uri.toString());
+        if (selection != null){
+            args.putString(LOADER_SELECTION, selection);
+        }
+        if (selectionArgs != null){
+            args.putString(LOADER_SELECTION_ARGS, selectionArgs);
+        }
+        if (uri != null){
+            return args;
+        }
+        return null;
+    }
+}
